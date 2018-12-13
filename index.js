f0237e51ca3cb0b4725a4770852fb9dcfdf86f00
Samuel Simonney <samuel@pulsar-informatique.com>
2018-12-13 09:54:46 +0100

    Ajout du style des différentes chansons

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function retourParole(callback) {
+function retourParole(chanson,callback) {
     var xmlhttp = new XMLHttpRequest(); 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           callback(xmlhttp.responseText);
         }
     };
-    xmlhttp.open("GET", "november.txt", true);
+    xmlhttp.open("GET", chanson+".txt", true);
     xmlhttp.send();
     }
 
     function callback(data) {
         paroles =  data.split('\n');
+        console.log(paroles);
      }
-     retourParole(callback);
     
   function sToTime(t) {
     return padZero(parseInt((t / (60)) % 60)) + ":" + 
@@ -24,6 +24,51 @@ function retourParole(callback) {
   function speak (phrase) {
     window.speechSynthesis.speak(new window.SpeechSynthesisUtterance(phrase))
   }
+
+  var e = document.getElementById("choix");
+  e.addEventListener('change', function(){
+    var choixUser = this.options[this.selectedIndex].value;
+
+    //Renitianilisation du style avant le test des chnaons 
+    document.getElementsByTagName("BODY")[0].className='';
+    document.getElementById('night').style.opacity='0';
+    document.getElementById('nyan').style.display='none';
+    document.getElementById('nyan2').style.display='none';
+    document.getElementById('nyan3').style.display='none';
+    document.getElementById('nyan4').style.display='none';
+    document.getElementById('rain_container').style.display='none';
+    document.getElementsByTagName("BODY")[0].style.animation='none';
+    console.log(choixUser);
+      switch(choixUser){
+        case 'titanic':
+        document.getElementsByTagName("BODY")[0].classList.add('sea');
+        break;
+        case 'rickroll':
+        document.getElementsByTagName("BODY")[0].classList.add('star');
+        document.getElementsByTagName("BODY")[0].classList.add('star_night');
+        document.getElementById('night').style.opacity='1';
+        break;
+        case 'november':
+        document.getElementsByTagName("BODY")[0].classList.add('rain_body');
+        document.getElementById('rain_container').style.display='block';
+        break;
+        case 'nyan':
+        
+        document.getElementsByTagName("BODY")[0].classList.add('nyan');
+        document.getElementById('nyan').style.display='block';
+       document.getElementById('nyan2').style.display='block';
+       document.getElementById('nyan3').style.display='block';
+       document.getElementById('nyan4').style.display='block';
+        document.getElementsByTagName("BODY")[0].style.animation='BGC 1s infinite';
+        break;
+
+      }
+    var audioPlayer=document.getElementById('audioSource');
+    audioPlayer.src=choixUser+'.mp3';
+    document.getElementById('audioPlay').load();
+    retourParole(choixUser,callback);
+  })
+
     var precedent='';
     var aud= document.getElementById('audioPlay');
     aud.ontimeupdate = function() {myFunction()};
